8a08f51878052811f6554e63134fbd7e60c92f7a
Fred Ma <fma1@terpmail.umd.edu>
2018-06-01 21:59:08 -0400

    Changed DockerContext to securely communicate with Docker

--- src/main/scala/devbook/DockerContext.scala
+++ src/main/scala/devbook/DockerContext.scala
@@ -1,6 +1,8 @@
 package DevBook
 
-import java.io.File
+import DevBook.Utils.synchronizedPrintln
+
+import java.io.{ File, PrintWriter, IOException }
 import scala.io.Source
 
 import scala.concurrent.duration.Duration
@@ -8,7 +10,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import java.util.concurrent.TimeUnit._
 import scala.concurrent._
 
-import scala.util.Try
+import scala.util.{ Try, Success, Failure }
 
 import com.github.dockerjava.api._
 import com.github.dockerjava.core._
@@ -33,10 +35,74 @@ object DockerContext {
   // Create configuration for Docker
   // Use the server address defined above
   // And use 1.26 API version
-  val config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+  var configBuilder = DefaultDockerClientConfig.createDefaultConfigBuilder()
     .withDockerHost(serverAddress)
     .withApiVersion("1.26")
-    .build();
+
+  // We're checking the DOCKER_TLS_VERIFY environment variable
+  // and if it exists we'll use TLS verification
+  // And then we'll DOCKER_CERT_CA, DOCKER_CERT_CERT, DOCKER_CERT_KEY
+  // from the environment and write those out to files
+  // and then we'll give that directory to the configuration
+  //
+  // One thing here is that I'm not handling any exceptions
+  // If there's an error opening or writing a file
+  // I want the error to propagate up the call stack
+  // and stop the program
+  if (System.getenv("DOCKER_TLS_VERIFY") != null) {
+    configBuilder = configBuilder.withDockerTlsVerify(true)
+
+    val ca_contents =
+      Try(sys.env("DOCKER_CERT_CA")) match {
+        case Success(ca_contents) =>
+          Some(ca_contents)
+        case Failure(_) => 
+          synchronizedPrintln("Error getting DOCKER_CERT_CA variable")
+          None
+      }
+
+    val cert_contents =
+      Try(sys.env("DOCKER_CERT_CERT")) match {
+        case Success(cert_contents) =>
+          Some(cert_contents)
+        case Failure(_) =>
+          synchronizedPrintln("Error getting DOCKER_CERT_CERT variable")
+          None
+      }
+
+    val key_contents =
+      Try(sys.env("DOCKER_CERT_KEY")) match {
+        case Success(key_contents) =>
+          Some(key_contents)
+        case Failure(_) =>
+          synchronizedPrintln("Error getting DOCKER_CERT_KEY variable")
+          None
+      }
+
+    if (!ca_contents.isEmpty && !cert_contents.isEmpty && !key_contents.isEmpty) {
+      val currWorkDir = System.getProperty("user.dir")
+
+      val createdDir = new File(s"$currWorkDir/.docker").mkdirs()
+      var printWriter = new PrintWriter(s"$currWorkDir/.docker/ca.pem")
+      printWriter.write(ca_contents.get)
+      if (printWriter.checkError()) throw new IOException()
+      printWriter.close
+
+      printWriter = new PrintWriter(s"$currWorkDir/.docker/cert.pem")
+      printWriter.write(cert_contents.get)
+      if (printWriter.checkError()) throw new IOException()
+      printWriter.close
+
+      printWriter = new PrintWriter(s"$currWorkDir/.docker/key.pem")
+      printWriter.write(key_contents.get)
+      if (printWriter.checkError()) throw new IOException()
+      printWriter.close
+
+      configBuilder = configBuilder.withDockerCertPath(s"$currWorkDir/.docker")
+    }
+  }
+
+  val config = configBuilder.build()
 
   // Create an instance of the Java DockerClient
   // https://github.com/docker-java/docker-java/wiki
