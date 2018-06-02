0a10989ec547fbce890173387e218eb63dc30ff1
Fred Ma <fma1@terpmail.umd.edu>
2018-06-01 21:59:59 -0400

    Changed FirebaseService to get credentials from environment

--- src/main/scala/devbook/FirebaseService.scala
+++ src/main/scala/devbook/FirebaseService.scala
@@ -1,13 +1,24 @@
 package DevBook
 
-import java.io.FileInputStream
+import java.io.{ File, FileInputStream, PrintWriter, IOException }
 import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.cloud.FirestoreClient
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 
 object FirebaseService {
-  private val serviceAccount = new FileInputStream(System.getProperty("user.dir") + "/serviceAccountKey.json")
+  private val serviceAccountPath = System.getProperty("user.dir") + "/serviceAccountKey.json"
+  private val serviceAccountFile = new File(serviceAccountPath)
+
+  if (!serviceAccountFile.exists()) {
+    val credentialsContents = sys.env("GOOGLE_APPLICATION_CREDENTIALS")
+    val printWriter = new PrintWriter(serviceAccountPath)
+    printWriter.write(credentialsContents)
+    if (printWriter.checkError()) throw new IOException()
+    printWriter.close
+  }
+
+  private val serviceAccount = new FileInputStream(serviceAccountPath)
   private val credentials = GoogleCredentials.fromStream(serviceAccount)
   private val options = new FirebaseOptions.Builder()
     .setCredentials(credentials)
